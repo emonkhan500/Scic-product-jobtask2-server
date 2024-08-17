16f243fbfa747fcdc447d0f796157fff33b8c538
emonkhan500 <khdilo47@gmail.com>
2024-08-17 11:29:30 +0600

    basic setup complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express =require('express')
+const cors =require('cors');
+require('dotenv').config();
+const app =express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req,res)=>{
+    res.send('assignment server is running')
+})
+
+app.listen(port,()=>{
+    console.log(`assignment server is running on port ${port}`)
+})
